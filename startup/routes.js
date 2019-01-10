bdae448afa2a898c07e00a4b2b096bfdd53a940d
jhony.lopez <jhony.lopez@hulu.com>
2019-01-10 16:12:40 -0500

    add middlewares to support json and urlencode

--- startup/routes.js
+++ startup/routes.js
@@ -1,9 +1,16 @@
 "use strict";
 
 const express = require('express');
+// const bodyParser = require('body-parser');
 const books = require('../routes/books');
 
 module.exports = function(app) {
+  // Express usa los siguientes middleware que estan basados en body parser
   app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  // app.use(bodyParser.json());  // to support JSON-encoded bodies
+  // app.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bodies
+  
   app.use('/api/books', books);
 }
