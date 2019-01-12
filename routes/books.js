9464a478ac37f4da4b45a89d42ab280e9cad21b8
jhony.lopez <jhony.lopez@hulu.com>
2019-01-11 22:39:55 -0500

    add some comment and fixes to patch method

--- routes/books.js
+++ routes/books.js
@@ -3,6 +3,9 @@
 const express = require('express');
 const router = express.Router();
 
+// Aqui agregamos los metodos
+// https://www.rfc-archive.org/getrfc?rfc=2068
+
 router.get('/', (req, res) => {
   const books = book ;
   res.send(books);
@@ -34,31 +37,43 @@ router.delete('/:id', (req, res) => {
 });
 
 // Actualiza solo los campos enviados, si no existe el recurso devuelve un error
-// https://tools.ietf.org/html/rfc7396
+// https://www.rfc-archive.org/getrfc?rfc=5789
+// https://www.rfc-archive.org/getrfc?rfc=7396
 router.patch('/:id', (req, res) => {
   const item = book.filter(book => book.code == req.params.id );
   if (item.length > 0) {
-    const resp = {...item[0]}
-    Object.keys(req.body).forEach( elem => {
-      if (req.body[elem] === null) {
-        delete resp[elem];
-      }
-      else {
-        resp[elem] = req.body[elem];
-      }
-    });
 
+    // función sugerida en la documentación
+    function mergePatch(target, patch) {
+      if (typeof(patch) === 'object') {
+        if (typeof(target) !== 'object') {
+          return {};
+        }
+        Object.keys(patch).forEach( key => {
+          if (patch[key] === null) {
+            if (target[key]) {
+              delete target[key];
+            }
+          }
+          else {
+            target[key] = mergePatch(target[key], patch[key]);
+          }
+        });
+        return target;
+      }
+      return patch;
+    }
+
+    const diff = mergePatch({...item[0]}, req.body);
     book = book.map(bo => {
-      if (bo.code == resp.code) {
-        return bo = resp;
+      if (bo.code == req.body.code) {
+        return bo = diff;
       } 
       return bo;
-      
-    });
-
-    return res.send(resp);
+    })
+    return res.send(diff);
   }
-  res.status(404).send()
+  res.status(404).send();
 });
 
 // Actualiza todo el recurso, si no existe se crea
